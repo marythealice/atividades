24a8604972a54d6a8fbf7aca526fe35490a9e7cb
Maria Alice <nantesmalice@gmail.com>
2025-08-18 16:44:56 -0400

    Termino do catalogo refatorado

--- catalogo_refatorado/script.js
+++ catalogo_refatorado/script.js
@@ -1,121 +1,114 @@
 // Declarando const produtos como array de objetos:
 
 const produtos = [
-    { nome: "Lip Tint Bruna Tavares", preco: 50.99, emEstoque: false, imagem: "/img/produto1.jpg" },
-    { nome: "Gloss Boca Rosa", preco: 39.99, emEstoque: true, imagem: "/img/produto2.jpg" },
-    { nome: "Lapis de Olhos Jequiti", preco: 29.99, emEstoque: true, imagem: "/img/produto3.jpg" },
-    { nome: "Body Splash - rose petals", preco: 99.00, emEstoque: true, imagem: "/img/produto4.jpg" }
-]
+    { nome: "Lip Tint Bruna Tavares", preco: 50.99, emEstoque: false, imagem: "./img/produto1.jpg" },
+    { nome: "Gloss Boca Rosa", preco: 39.99, emEstoque: true, imagem: "./img/produto2.jpg" },
+    { nome: "Lapis de Olhos Jequiti", preco: 29.99, emEstoque: true, imagem: "./img/produto3.jpg" },
+    { nome: "Body Splash - rose petals", preco: 99.00, emEstoque: true, imagem: "./img/produto4.jpg" }
+];
 
-// Funcao que recebe array de produtos
+const botaoAplicarDesconto = document.getElementById("btn-desconto");
+const botarMostrarDisponiveis = document.getElementById("btn-filtrar");
+const botaoMostrarTodos = document.getElementById("btn-mostrar-todos");
 
-function RenderizarProdutos(lista) {
+// Funcao que recebe array de produtos e o transforma produtos renderizados
+
+function renderizarProdutos(listaDeProdutos) {
     const grid = document.getElementById("product-grid")
-    grid.innerHTML = "" // limpando o grid
+    grid.innerHTML = ""; // limpando o grid
 
-    lista.forEach(produto => {
+    listaDeProdutos.forEach(produto => {
 
-        // criando o card
-        const card = document.createElement("div")
-        card.classList.add("card")
+        // criando elemento div com class card
+        const card = document.createElement("div");
+        card.classList.add("card");
 
-        // criando elemento para nome
-        const nome = document.createElement("h3")
-        nome.textContent = produto.nome // atribuindo o nome presente no objeto
+        // preenchendo o conteudo do card com innerHTML dentro do for each para renderizar todos os produtos presentes no array
 
-        // Preco formatado
-        /** @type {number} */
-        const preco = document.createElement("p")
-        preco.textContent = produto.preco.toLocaleString("pt-BR", {
-            style: "currency",
-            currency: "BRL"
-        })
+        card.innerHTML = `
+        <img src="${produto.imagem}" alt="${produto.nome}">
+        <h3>${produto.precoOriginal ? `<s style="color: gray; font-size: 20px;">R$ ${produto.precoOriginal.toFixed(2)}</s> ` : ''} R$ ${produto.preco.toFixed(2)}</h3>
+        <p class="${produto.emEstoque ? "" : "out-of-stock"}">
+            ${produto.emEstoque ? "Em estoque" : "Esgotado"}
+        </p>
 
-        const imagem = document.createElement("img");
-        imagem.src = produto.imagem
-        imagem.alt = produto.nome
+        `;
 
-        const status = document.createElement("p")
-        status.textContent = produto.emEstoque ? "Em estoque" : "Esgotado"
+        grid.appendChild(card);
 
-        if (!produto.emEstoque) {
-            status.classList.add("out-of-stock")
-        }
-
-        card.appendChild(nome)
-        card.appendChild(preco)
-        card.appendChild(imagem)
-        card.appendChild(status)
-
-        grid.appendChild(card)
     });
+}
 
+// Funcao que calcula o valor total de produtos em um array
 
+function calcularValorDoEstoque(listaDeProdutos) {
 
+    const total = listaDeProdutos
+        .filter(produto => produto.emEstoque)
+        .reduce((soma, produto) => soma + produto.preco, 0);
 
+    const valorEstoque = document.getElementById("valor-total-estoque");
+    valorEstoque.textContent = `R$ ${total.toFixed(2)}`;
 
 }
 
-// console.table(produtos)
+// Funcao que aplica desconto nos precos de um objeto e retorna um novo array com o preco original e o com desconto
 
-// // Calcular e exibir no console o valor total de itens em estoque utilizando filter + reduce
+function aplicarDesconto(listaDeProdutos) {
+    return listaDeProdutos.map(produto => ({
+        ...produto,
+        precoOriginal: produto.preco,
+        preco: Number((produto.preco * 0.9).toFixed(2))
+    }))
+}
 
-// const valorTotalProdutosComDesconto = produtos.filter(produtos => produtos.emEstoque == true).reduce((soma, produto) => soma + produto.preco, 0)
 
-// console.log("Valor total dos produtos em estoque:")
-// console.log(valorTotalProdutosComDesconto.toFixed(2))
+// Funcao que retornar um novo array com apenas os produtos disponiveis
 
-// // Criar um novo array de objetos com 10% de desconto em cada preço usando map e exibir com console.log
+function retornaProdutosDisponiveis(listaDeProdutos) {
+    return listaDeProdutos.filter(produto => produto.emEstoque)
+}
 
-// const produtosComDesconto = produtos.map(produto => ({
-//     ...produto,
-//     preco: Number((produto.preco * 0.9).toExponential(2))
-// }))
 
-// console.log(produtosComDesconto)
+// Funcao que muda o conteudo da div com id log-loops para mostrar a iteracao de todos os produtos presentes no array inicial
 
-// // Filtrar apenas produtos disponíveis com filter e exibir no console
+function demonstrarLacosDeProdutos(listaDeProdutos) {
+    const divlogs = document.getElementById("log-loops");
+    let resultado = "";
+    resultado += "Demonstracao de loop for..of\n"
+    for (const produto of listaDeProdutos) {
+        resultado += ` ${produto.nome}`
+    };
 
-// const produtosEmEstoque = produtosComDesconto.filter(produto => produto.emEstoque)
+    divlogs.textContent = resultado;
+}
 
-// console.log("Produtos com desconto em estoque:")
+/* Colocando todas as funcoes dentro do evento DOMContentLoaded 
+para que a pagina nao precise recarregar para exibir as funcoes
+manejadas pelos botoes
+*/
 
-// console.table(produtosEmEstoque)
+document.addEventListener("DOMContentLoaded", () => {
+    renderizarProdutos(produtos);
+    calcularValorDoEstoque(produtos);
+    demonstrarLacosDeProdutos(produtos);
 
-// // Iterar sobre produtos usando:
+    botaoAplicarDesconto.addEventListener('click', () => {
+        const produtosComDesconto = aplicarDesconto(produtos);
+        renderizarProdutos(produtosComDesconto);
+        calcularValorDoEstoque(produtosComDesconto);
+    })
 
-// //for...of para imprimir o nome de cada 
+    botarMostrarDisponiveis.addEventListener('click', () => {
+        const produtosDisponiveis = retornaProdutosDisponiveis(produtos);
+        renderizarProdutos(produtosDisponiveis)
+        calcularValorDoEstoque(produtosDisponiveis)
+    })
 
-// console.log("Produtos disponiveis")
+    botaoMostrarTodos.addEventListener('click', () => {
+        renderizarProdutos(produtos)
+        calcularValorDoEstoque(produtos)
+    })
 
-// for (const produto of produtos) {
-//     console.log(`${produto.nome}`)
-// }
-
-// // while para percorrer o array e exibir cada nome
-
-// console.log("Produtos disponiveis")
-
-// let i = 0
-
-// while (i < produtos.length) {
-//     console.log(`${produtos[i].nome}`)
-//     i++
-// }
-
-// let restanteDosProdutos = produtos.length
-
-// // do...while para fazer uma contagem regressiva de quantos itens faltam processar
-
-// console.log("Contagem regressiva do processamento dos produtos")
-
-// do {
-//     console.log(`Faltam ${restanteDosProdutos} para processar`)
-//     restanteDosProdutos--
-// } while (restanteDosProdutos > 0)
-
-// // Chamar funcoes depois que html e css ja tiverem sido carregados
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('O dom esta pronto')
 })
+
